e64792c90c91a927e0fe5d32507bcc917241653d
Jake Drew <jakedrew@outlook.com>
2018-07-11 09:38:29 +0100

    Delete http.post.js

--- src/http.post.js
+++
@@ -1,7 +0,0 @@
-fn.post = function (url, data) {
-    var request = new XMLHttpRequest();
-    request.open('POST', url, true);
-    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-    request.send(data);
-};
-  
